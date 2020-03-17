6b42e0af71e7a8213489794320df26c0b7382fee
Harrison McCarty <hmccarty@pm.me>
2020-03-17 15:41:14 -0400

    added tree generation and obstacle avoidance

--- rrt.py
+++ rrt.py
@@ -1,5 +1,17 @@
 import visualizer as vis 
 import random as rdm
+import pygame
+import time
+import math
+
+width = 1024
+height = 576
+
+def dist(start, end):
+    x_comp = end[0] - start[0]
+    y_comp = end[1] - start[1]
+
+    return math.sqrt(math.pow(x_comp, 2) + math.pow(y_comp, 2))
 
 class Node:
     def __init__(self, x, y):
@@ -10,16 +22,71 @@ class Node:
         self.children.append(child)
 
 class RRT:
-    def __init__(self, root, step_size, screen_x, screen_y):
+    def __init__(self, root, step_size):
         self.root = root
         self.step_size = step_size
-        self.screen_x = screen_x
-        self.screen_y = screen_y
+        self.obstacles = []
 
-root = Node(150, 150)
-root.add_child(Node(0, 0))
+    def find_closest_node(self, node, pnt):
+        for child in node.children:
+            dist_val = dist(child.pos, pnt)
+            if dist_val < self.min_val:
+                self.min_val = dist_val
+                self.min_node = child
+            self.find_closest_node(child, pnt)
 
+    def add_obstacle(self, obstacle):
+        self.obstacles.append(obstacle)
 
+    def find_closest(self, pnt):
+        self.min_val = dist(self.root.pos, pnt)
+        self.min_node = self.root
+        
+        self.find_closest_node(self.root, pnt)
+
+    def step(self):
+        x = rdm.randint(0, width)
+        y = rdm.randint(0, height)
+
+        self.find_closest((x,y))
+        parent_node = self.min_node
+
+        y_diff = y - parent_node.pos[1]
+        x_diff = x - parent_node.pos[0]
+        
+        if x_diff == 0:
+            if y_diff > 0:
+                angle = math.pi / 2
+            else:
+                angle = 3 * math.pi / 2
+        elif y_diff == 0:
+            if x_diff > 0:
+                angle = 0
+            else:
+                angle = math.pi
+        else:
+            angle = math.atan(y_diff / x_diff)
+       
+        mag = self.step_size
+        if self.min_val < self.step_size:
+            mag = self.min_val
+
+        new_x = parent_node.pos[0] + int(mag * math.cos(angle))
+        new_y = parent_node.pos[1] + int(mag * math.sin(angle))
+
+        collision = False
+        for obstacle in self.obstacles:
+            if obstacle.collidepoint(new_x, new_y):
+                collision = True
+                break
+
+        if not collision:
+            parent_node.add_child(Node(new_x, new_y))
+
+root = Node(150,150)
+rrt = RRT(root, 25)
+rrt.add_obstacle(pygame.Rect(600, 100, 200, 100))
+rrt.add_obstacle(pygame.Rect(0, 400, 700, 50))
 while 1:
-    vis.plot_graph(root)
-
+    vis.plot_graph(rrt)
+    rrt.step()
--- visualizer.py
+++ visualizer.py
@@ -6,7 +6,9 @@ node_radius = 5
 edge_width = 3
 node_color = 1, 84, 143
 edge_color = 0, 0, 0
+obstacle_color = 0, 0, 0
 
+bg_color = 255, 255, 255
 size = width, height = 1024, 576
 white = 255, 255, 255
 
@@ -18,16 +20,23 @@ def draw_node(node):
 def draw_edge(start, end):
     pygame.draw.line(screen, edge_color, start.pos, end.pos, edge_width)
 
-def plot_node(node):
+def draw_obstacle(obstacle):
+    shape = obstacle.copy()
+    shape.inflate_ip(-15, -15)
+    pygame.draw.rect(screen, obstacle_color, shape) 
+
+def plot_tree(node):
     for child in node.children:
         draw_edge(node, child)
-        plot_node(child)
+        plot_tree(child)
 
     draw_node(node)
 
-def plot_graph(root):
+def plot_graph(graph):
     screen.fill(white)
     
-    plot_node(root)
+    plot_tree(graph.root)
+    for obstacle in graph.obstacles:
+        draw_obstacle(obstacle)
     
     pygame.display.flip()
