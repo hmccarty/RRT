2c826ce7537ad6c2e83a5c31af631c0e4d8e2cf7
Harrison McCarty <hmccarty@pm.me>
2020-03-19 03:26:31 -0400

    fixed visualization redundancies and errors

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import rrt, sys, pygame, time
 from PIL import Image
 
+COLOR_NODES = False
+
 NODE_RADIUS = 4
 EDGE_WIDTH = 3
 EDGE_COLOR = 0, 0, 0
@@ -21,47 +23,31 @@ screen = pygame.display.set_mode(PIL_image.size)
 background_image = pygame.image.load(file_name).convert()
 
 def plot_graph(graph):
-    screen.blit(background_image, [0, 0])
-    
-    draw_goal(graph.goal)
     plot_node(graph.root)
-    
-    pygame.display.flip()
+    pygame.display.update()
 
 def plot_path(graph):
     if graph.end_node is None:
         return
 
-    inactive_path = [graph.end_node]
-    active_path = []
+    path = [graph.end_node]
 
     node = graph.end_node.prev_node
     while True:
-        inactive_path.insert(0, node)
+        path.insert(0, node)
         if node.prev_node is None:
             break
         node = node.prev_node
     
-    screen.blit(background_image, [0, 0])
-    draw_goal(graph.goal)
-    plot_node(graph.root)
     start_pos = node.pos
     draw_circle(START_NODE_COLOR, start_pos, PATH_NODE_RADIUS)
-    pygame.display.flip()
+    pygame.display.update()
 
-    while inactive_path:
-        screen.blit(background_image, [0, 0])
-        draw_goal(graph.goal)
-        plot_node(graph.root)
-        draw_circle(START_NODE_COLOR, start_pos, PATH_NODE_RADIUS)
-        
-        active_path.append(inactive_path.pop(0))
+    for node in path:
+        if node.prev_node is not None:
+            draw_path(node.prev_node, node)
 
-        for node in active_path:
-            if node.prev_node is not None:
-                draw_path(node.prev_node, node)
-
-        pygame.display.flip()
+        pygame.display.update()
         time.sleep(0.5)
 
 def plot_node(node):
@@ -86,13 +72,19 @@ def draw_goal(goal):
 def draw_circle(color, pnt, radius):
     pygame.draw.circle(screen, color, pnt, radius)
 
-if __name__ == "__main__":
-    root = rrt.Node(300,150)
-    graph = rrt.Graph(root, 50, PIL_image.load(), PIL_image.size[0], PIL_image.size[1])
+root = rrt.Node(300,150)
+graph = rrt.Graph(root, 50, PIL_image.load(), PIL_image.size[0], PIL_image.size[1])
 
-    for x in range(750):
+screen.blit(background_image, [0, 0])
+draw_goal(graph.goal)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    while graph.end_node is None:
+        graph.add_node() 
         plot_graph(graph)
-        graph.add_node()
-        if graph.end_node is not None:
-            break
 
+        if graph.end_node is not None:
+            graph.find_path() 
+            plot_path(graph) 
